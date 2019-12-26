6a4888b9d2b8f2fa4a22bc4733a6478d1bcffa6d
Melissa Real <melissa.real@hotmail.com>
2019-12-26 10:54:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your solution here!
 
-const append = ["Milo", "Otis", "Garfield"]
-const prepend = ["Milo", "Otis", "Garfield"]
-const removeLast = ["Milo", "Otis", "Garfield"]
-const removeFirst = ["Milo", "Otis", "Garfield"]
+const append = ["Milo", "Otis", "Garfield"].push("Odie")
+const prepend = ["Milo", "Otis", "Garfield"].unshift("Odie")
+const removeLast = ["Milo", "Otis", "Garfield"].pop();
+const removeFirst = ["Milo", "Otis", "Garfield"].shift();
 
 
 // function append(petArr){
