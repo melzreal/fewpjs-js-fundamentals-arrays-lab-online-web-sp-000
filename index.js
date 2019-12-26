ecfccc762d88953075a31bde401acd5892e779a1
Melissa Real <melissa.real@hotmail.com>
2019-12-26 10:55:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,3 @@ const removeFirst = ["Milo", "Otis", "Garfield"]
 removeFirst.shift();
 
 
-// function append(petArr){
-// return petArr.push("Odie")
-// }
-
-// function prepend(petArr){
-//   return petArr.unshift("Odie")
-// }
-
-
-// function removeLast(petArr){
-//   return petArr.pop();
-// }
-
-// function removeFirst(petArr){
-//   return petArr.shift();
-// }
